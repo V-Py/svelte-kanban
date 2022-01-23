2acf8f38c0f864e0ec7b0c6876e7612ae5daa665
Valentin Py <valentin.pyb@gmail.com>
2022-01-23 09:46:13 +0100

    colum resize observer

--- src/lib/stores/store.js
+++ src/lib/stores/store.js
@@ -1,10 +1,7 @@
 import { writable } from 'svelte/store';
 import {Lang} from '$lib/class/Lang';
 
-export const card_width = writable(200);
-export const card_height = writable(120);
 export const number_of_slots = writable(0);
-
 export const main_width = writable('');
 export const main_height = writable('');
 export const columns = writable([]);
