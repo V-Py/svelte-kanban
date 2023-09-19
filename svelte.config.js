4a14e655a22040b2c62b12cf57dc34b3a3dfda08
dev-guy <devguy@devguy.com>
2023-09-19 09:00:38 -0700

    checkpoint

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
 
 	kit: {
 		alias: {
-			$store: './src/lib/stores',
+			$stores: 'src/lib/stores',
 		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
